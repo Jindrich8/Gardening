cddab85897e67e10e9aaf4e5c75aa4d5ed531130
Jindřich Svatoň <jindrich.svaton@gmail.com>
2024-11-12 09:57:54 +0100

    Edit device and drop_zone scripts

--- modules/device.js
+++ modules/device.js
@@ -1,20 +1,36 @@
 const mobileMedia = window.matchMedia("only screen and (max-width: 700px)");
-const uniqueObservers = new Map();
+const touchScreenMedia = window.matchMedia("(pointer: coarse)");
+const mobileUniqueObservers = new Map();
+const touchUniqueObservers = new Map();
 
 mobileMedia.addEventListener('change', (e) => {
     const isMobile = e.matches;
-    for (const key of uniqueObservers.keys()) {
+    for (const key of mobileUniqueObservers.keys()) {
         key(isMobile);
+    }
+});
+
+touchScreenMedia.addEventListener('change', (e) => {
+    const isTouch = e.matches;
+    for (const key of touchUniqueObservers.keys()) {
+        key(isTouch);
     }
 });
 
 export const device = ({
     isMobile: () => mobileMedia.matches,
+    isTouch: () => touchScreenMedia.matches,
     addDeviceChangeListener: (callback) => {
-        uniqueObservers.set(callback, true);
+        mobileUniqueObservers.set(callback, true);
     },
     removeDeviceChangeListener: (callback) => {
-        uniqueObservers.delete(callback);
+        mobileUniqueObservers.delete(callback);
+    },
+    addMouseChangeListener: (callback) => {
+        touchUniqueObservers.set(callback, true);
+    },
+    removeMouseChangeListener: (callback) => {
+        touchUniqueObservers.delete(callback);
     }
 });
 
