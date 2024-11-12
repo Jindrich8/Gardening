9b58e154115e4e1afe16c1604500f9e96a394c6c
Jindřich Svatoň <jindrich.svaton@gmail.com>
2024-11-12 11:00:57 +0100

    Correct path in temporal header and footer synchronizer

--- base.js
+++ base.js
@@ -12,7 +12,7 @@ function getBetween(text, tag) {
     const end = text.indexOf(endTag);
     return text.substring(start, end);
 }
-fetch(window.location.origin).then((resp) => resp.text())
+fetch(window.location.origin + '/Gardening/').then((resp) => resp.text())
     .then((text) => {
         document.querySelector('header').innerHTML = getBetween(text, 'header');
         document.querySelector('footer').innerHTML = getBetween(text, 'footer');
