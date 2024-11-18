1839f077341eea7774c24cccec858654425b45d3
Jindřich Svatoň <jindrich.svaton@gmail.com>
2024-11-18 21:29:19 +0100

    Switch to optimized icon font and fix pages

--- modules/drop_zone.js
+++ modules/drop_zone.js
@@ -68,7 +68,7 @@ const dictOptions = {
      */
     dictMaxFilesExceeded: "Nelze nahrát více souborů.",
 
-}
+};
 
 const previewTemplate = /*html*/`
 <div class="dz-preview dz-image-preview"> 
@@ -77,19 +77,21 @@ const previewTemplate = /*html*/`
     </div> 
     <div class="dz-details"> 
         <div class="dz-size">
-            <span data-dz-size="">
-                </div> <div class="dz-filename">
-                    <span data-dz-name=""></span>
-                </div>
-                 </div> 
-                  <div class="dz-error-message">
-                        <span data-dz-errormessage=""></span>
-                        </div> <div class="dz-success-mark"> 
-                            <svg width="54" height="54" fill="#fff">
-                                <path d="m10.207 29.793 4.086-4.086a1 1 0 0 1 1.414 0l5.586 5.586a1 1 0 0 0 1.414 0l15.586-15.586a1 1 0 0 1 1.414 0l4.086 4.086a1 1 0 0 1 0 1.414L22.707 42.293a1 1 0 0 1-1.414 0L10.207 31.207a1 1 0 0 1 0-1.414Z"></path></svg> </div> <div class="dz-error-mark"> <svg width="54" height="54" fill="#fff"><path d="m26.293 20.293-7.086-7.086a1 1 0 0 0-1.414 0l-4.586 4.586a1 1 0 0 0 0 1.414l7.086 7.086a1 1 0 0 1 0 1.414l-7.086 7.086a1 1 0 0 0 0 1.414l4.586 4.586a1 1 0 0 0 1.414 0l7.086-7.086a1 1 0 0 1 1.414 0l7.086 7.086a1 1 0 0 0 1.414 0l4.586-4.586a1 1 0 0 0 0-1.414l-7.086-7.086a1 1 0 0 1 0-1.414l7.086-7.086a1 1 0 0 0 0-1.414l-4.586-4.586a1 1 0 0 0-1.414 0l-7.086 7.086a1 1 0 0 1-1.414 0Z"></path>
-                                </svg> 
-                                </div> 
-                                </div>`;
+            <span data-dz-size=""></span>
+        </div> 
+        <div class="dz-filename">
+            <span data-dz-name=""></span>
+        </div>
+    </div> 
+    <div class="dz-error-message">
+        <span data-dz-errormessage=""></span>
+    </div> 
+    <div class="dz-success-mark"> 
+        <svg width="54" height="54" fill="#fff">
+            <path d="m10.207 29.793 4.086-4.086a1 1 0 0 1 1.414 0l5.586 5.586a1 1 0 0 0 1.414 0l15.586-15.586a1 1 0 0 1 1.414 0l4.086 4.086a1 1 0 0 1 0 1.414L22.707 42.293a1 1 0 0 1-1.414 0L10.207 31.207a1 1 0 0 1 0-1.414Z"></path></svg> </div> <div class="dz-error-mark"> <svg width="54" height="54" fill="#fff"><path d="m26.293 20.293-7.086-7.086a1 1 0 0 0-1.414 0l-4.586 4.586a1 1 0 0 0 0 1.414l7.086 7.086a1 1 0 0 1 0 1.414l-7.086 7.086a1 1 0 0 0 0 1.414l4.586 4.586a1 1 0 0 0 1.414 0l7.086-7.086a1 1 0 0 1 1.414 0l7.086 7.086a1 1 0 0 0 1.414 0l4.586-4.586a1 1 0 0 0 0-1.414l-7.086-7.086a1 1 0 0 1 0-1.414l7.086-7.086a1 1 0 0 0 0-1.414l-4.586-4.586a1 1 0 0 0-1.414 0l-7.086 7.086a1 1 0 0 1-1.414 0Z"></path>
+        </svg> 
+    </div> 
+</div>`;
 
 
 const forms = document.querySelectorAll("form:has(.dropzone)");
